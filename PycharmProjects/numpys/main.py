09d128e358e8c14d633a36b0a29fe4a6befb692c
Adarsh Pandey <Adarsh.Pandey@BRILLIO.COM>
2022-03-20 00:40:36 +0530

    new

--- PycharmProjects/numpys/main.py
+++ PycharmProjects/numpys/main.py
@@ -12,11 +12,16 @@ data=json.load(f)
 print(data)'''
 
 with open('learn.json') as json_file:
-    data = json.load(json_file)
+    '''data = json.load(json_file)
     print(data)
     print(type(data))
     json_object = json.dumps(data)
     print(json_object)
-    print(data.keys())
+    print(json_object["name"])'''
+    data=json.load(json_file)
+    print(data)
+    for i in data:
+        print(i['name'])
+
 
 
